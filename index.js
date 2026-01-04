5cbc2dce35b120705e15f3b571df0f631701574e
Abhishek <srivastavaabhishek.8055@gmail.com>
2026-01-05 04:27:50 +0530

    import socket.io and set up initial configuration

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.send("Hey, I'm working!")
+    res.sendFile("index.html");
+})
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    })
 })
 
 server.listen(3000, () => {
